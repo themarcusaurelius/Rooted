1b8220c99292888c1231fb9f4d31d36ebfd7d22d
Andrea Halloran <andreahalloran01@gmail.com>
2018-09-24 19:21:14 -0500

    scroll lock feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyDW7VVTo0QIJkHrYXQX_ywKUBHc44AfXGs",
@@ -22,231 +22,290 @@ $(document).ready(function () {
             var value = $(this).val();
             var text = $(this).find('option:selected').text();
         })
-     
-    
 
-    
-    $("#submit").click(function () {
-        let company = $("#company-input").val();
 
-        console.log("click");
 
-        let newCompany = {
-            company: $("#company-input").val()
+
+        $("#submit").click(function () {
+            let company = $("#company-input").val();
+
+            console.log("click");
+
+            let newCompany = {
+                company: $("#company-input").val()
+            };
+
+            console.log(selectedValue);
+        })
+
+
+        // Push to Firebase
+        database.ref("/Company").push({
+            company: selectedValue,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("#company-input").val().trim();
+        console.log(selectedValue);
+
+
+        //HEADER and MENU FUNCTIONS
+
+        var header = $('.header');
+        var menu = $('.menu');
+        var menuActive = true;
+        var map;
+
+        setHeader();
+
+        $(window).on('resize', function () {
+            setHeader();
+
+            setTimeout(function () {
+                $(window).trigger('resize.px.parallax');
+            }, 375);
+        });
+
+        $(document).on('scroll', function () {
+            setHeader();
+        });
+
+        initMenu();
+        initGoogleMap();
+
+
+        ///////////////Set Header\\\\\\\\\\\\\\\\
+
+        function setHeader() {
+            if ($(window).scrollTop() > 127) {
+                header.addClass('scrolled');
+            } else {
+                header.removeClass('scrolled');
+            }
         };
 
-        console.log(selectedValue);
-    })
-    
 
-    // Push to Firebase
-    database.ref("/Company").push({
-        company: selectedValue, 
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+        ///////////////Set Menu\\\\\\\\\\\\\\\\\
 
-    $("#company-input").val().trim();
-    console.log(selectedValue);
-    
-   
-   //HEADER and MENU FUNCTIONS
+        function initMenu() {
+            if ($('.hamburger').length && $('.menu').length) {
+                var hamb = $('.hamburger');
+                var close = $('.menu_close_container');
 
-    var header = $('.header');
-    var menu = $('.menu');
-    var menuActive = true;
-    var map;
+                hamb.on('click', function () {
+                    if (!menuActive) {
+                        openMenu();
+                    } else {
+                        closeMenu();
+                    }
+                });
 
-    setHeader();
+                close.on('click', function () {
+                    if (!menuActive) {
+                        openMenu();
+                    } else {
+                        closeMenu();
+                    }
+                });
 
-    $(window).on('resize', function () {
-        setHeader();
 
-        setTimeout(function () {
-            $(window).trigger('resize.px.parallax');
-        }, 375);
-    });
-
-    $(document).on('scroll', function () {
-        setHeader();
-    });
-
-    initMenu();
-    initGoogleMap();
-
-      
-///////////////Set Header\\\\\\\\\\\\\\\\
-
-    function setHeader() {
-        if ($(window).scrollTop() > 127) {
-            header.addClass('scrolled');
-        } else {
-            header.removeClass('scrolled');
+            }
         }
-    };
 
-    
-///////////////Set Menu\\\\\\\\\\\\\\\\\
-    
-    function initMenu() {
-        if ($('.hamburger').length && $('.menu').length) {
-            var hamb = $('.hamburger');
-            var close = $('.menu_close_container');
+        function openMenu() {
+            menu.addClass('active');
+            menuActive = true;
+        }
 
-            hamb.on('click', function () {
-                if (!menuActive) {
-                    openMenu();
-                } else {
-                    closeMenu();
+        function closeMenu() {
+            menu.removeClass('active');
+            menuActive = false;
+        }
+
+        ///////////////Google Map\\\\\\\\\\\\\\\\
+        function initGoogleMap() {
+            var myLatlng = new google.maps.LatLng(30.2672, -97.7431);
+
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 12,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                draggable: true,
+                scrollwheel: true,
+                zoomControl: true,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.RIGHT_CENTER
+                },
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                rotateControl: true,
+                fullscreenControl: true,
+                styles: [{
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                        "color": "#ffeba1"
+                    }]
+                }]
+            }
+
+            // Initialize a map with options
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+            // Re-center map after window resize
+            google.maps.event.addDomListener(window, 'resize', function () {
+                setTimeout(function () {
+                    google.maps.event.trigger(map, "resize");
+                    map.setCenter(myLatlng);
+                }, 1400);
+            });
+        }
+
+        var map;
+
+        var markerData = [{
+                lat: 30.401081,
+                lng: -97.719290,
+                zoom: 14,
+                name: "Amazon"
+            },
+            {
+                lat: 30.432487,
+                lng: -97.735995,
+                zoom: 14,
+                name: "Apple"
+            },
+            {
+                lat: 30.309885,
+                lng: -97.742205,
+                zoom: 14,
+                name: "Bumble"
+            },
+            {
+                lat: 30.488424,
+                lng: -97.670435,
+                zoom: 14,
+                name: "Dell"
+            },
+            {
+                lat: 30.269091,
+                lng: -97.745817,
+                zoom: 14,
+                name: "Google"
+            },
+            {
+                lat: 30.404769,
+                lng: -97.721534,
+                zoom: 14,
+                name: "Homeaway"
+            },
+            {
+                lat: 30.393951,
+                lng: -97.724021,
+                zoom: 14,
+                name: "IBM"
+            },
+            {
+                lat: 30.363058,
+                lng: -97.793808,
+                zoom: 14,
+                name: "Indeed"
+            },
+            {
+                lat: 30.262696,
+                lng: -97.793318,
+                zoom: 14,
+                name: "Intel"
+            },
+        ];
+
+        function initialize() {
+            map = new google.maps.Map(document.getElementsByClassName('map'), {
+                zoom: 3,
+                center: {
+                    lat: 10,
+                    lng: 10
                 }
             });
-
-            close.on('click', function () {
-                if (!menuActive) {
-                    openMenu();
-                } else {
-                    closeMenu();
-                }
+            markerData.forEach(function (data) {
+                var newmarker = new google.maps.Marker({
+                    map: map,
+                    position: {
+                        lat: data.lat,
+                        lng: data.lng
+                    },
+                    title: data.name
+                });
+                jQuery("#search_form_select1").append('<option value="' + [data.lat, data.lng, data.zoom].join('|') + '">' + data.name + '</option>');
             });
 
-
         }
-    }
 
-    function openMenu() {
-        menu.addClass('active');
-        menuActive = true;
-    }
+        google.maps.event.addDomListener(window, 'load', initialize);
 
-    function closeMenu() {
-        menu.removeClass('active');
-        menuActive = false;
-    }
-
-///////////////Google Map\\\\\\\\\\\\\\\\
-function initGoogleMap() {
-    var myLatlng = new google.maps.LatLng(30.2672, -97.7431);
-    
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        draggable: true,
-        scrollwheel: true,
-        zoomControl: true,
-        zoomControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_CENTER
-        },
-        mapTypeControl: true,
-        scaleControl: true,
-        streetViewControl: true,
-        rotateControl: true,
-        fullscreenControl: true,
-        styles: [{
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ffeba1"
-            }]
-        }]
-    }
-
-    // Initialize a map with options
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    // Re-center map after window resize
-    google.maps.event.addDomListener(window, 'resize', function () {
-        setTimeout(function () {
-            google.maps.event.trigger(map, "resize");
-            map.setCenter(myLatlng);
-        }, 1400);
-    });
-}
-    
-    var map;
-
-    var markerData= [
-        {lat: 30.401081 , lng: -97.719290 , zoom: 14 , name: "Amazon"},
-        {lat: 30.432487 , lng: -97.735995 , zoom: 14 , name: "Apple"},
-        {lat: 30.309885 , lng: -97.742205 , zoom: 14 , name: "Bumble"},
-        {lat: 30.488424 , lng: -97.670435 , zoom: 14 , name: "Dell"},
-        {lat: 30.269091 , lng: -97.745817 , zoom: 14 , name: "Google"},
-        {lat: 30.404769 , lng: -97.721534 , zoom: 14 , name: "Homeaway"},
-        {lat: 30.393951 , lng: -97.724021 , zoom: 14 , name: "IBM"},
-        {lat: 30.363058 , lng: -97.793808 , zoom: 14 , name: "Indeed"},
-        {lat: 30.262696 , lng: -97.793318 , zoom: 14 , name: "Intel"},
-    ];
-
-    function initialize() {
-        map = new google.maps.Map(document.getElementsByClassName('map'), {
-           zoom: 3,
-           center: {lat: 10, lng: 10}
-       });
-       markerData.forEach(function(data) {
-           var newmarker= new google.maps.Marker({
-               map:map,
-               position:{lat:data.lat, lng:data.lng},
-               title: data.name
-           });
-           jQuery("#search_form_select1").append('<option value="'+[data.lat, data.lng,data.zoom].join('|')+'">'+data.name+'</option>');
-       });
-
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$(document).on('change','search_form_select1',function() {
-   var latlngzoom = $(this).val().split('|');
-   var newzoom = 1*latlngzoom[2],
-   newlat = 1*latlngzoom[0],
-   newlng = 1*latlngzoom[1];
-   map.setZoom(newzoom);
-   map.setCenter({lat:newlat, lng:newlng});
-});
+        $(document).on('change', 'search_form_select1', function () {
+            var latlngzoom = $(this).val().split('|');
+            var newzoom = 1 * latlngzoom[2],
+                newlat = 1 * latlngzoom[0],
+                newlng = 1 * latlngzoom[1];
+            map.setZoom(newzoom);
+            map.setCenter({
+                lat: newlat,
+                lng: newlng
+            });
+        });
 
 
 
-    function myFunction() {
-        var elmnt = document.getElementById("content");
-        elmnt.scrollIntoView();
-    }
-
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 10
-        },
-        draggable: true,
-        map: map
-    });
-
-///////////////Change Google Maps To Selected Company\\\\\\\\\\\\
-  
-
-///////////////Scroll Feature\\\\\\\\\\\\\\\\
-
-    window.smoothScroll = function(target) {
-        var scrollContainer = target;
-        do { //find scroll container
-            scrollContainer = scrollContainer.parentNode;
-            if (!scrollContainer) return;
-            scrollContainer.scrollTop += 1;
-        } while (scrollContainer.scrollTop == 0);
-
-        var targetY = 0;
-        do { //find the top of target relatively to the container
-            if (target == scrollContainer) break;
-            targetY += target.offsetTop -  18;
-        } while (target = target.offsetParent);
-
-        scroll = function(c, a, b, i) {
-            i++; if (i > 30) return;
-            c.scrollTop = a + (b - a) / 30 * i;
-            setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        function myFunction() {
+            var elmnt = document.getElementById("content");
+            elmnt.scrollIntoView();
         }
-        // start scrolling
-        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-    }
-});
+
+        var marker = new google.maps.Marker({
+            position: map.getCenter(),
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10
+            },
+            draggable: true,
+            map: map
+        });
+
+        ///////////////Change Google Maps To Selected Company\\\\\\\\\\\\
+
+
+        ///////////////Scroll Feature\\\\\\\\\\\\\\\\
+
+        window.smoothScroll = function (target) {
+            var scrollContainer = target;
+            do { //find scroll container
+                scrollContainer = scrollContainer.parentNode;
+                if (!scrollContainer) return;
+                scrollContainer.scrollTop += 1;
+            } while (scrollContainer.scrollTop == 0);
+
+            var targetY = 0;
+            do { //find the top of target relatively to the container
+                if (target == scrollContainer) break;
+                targetY += target.offsetTop - 18;
+            } while (target = target.offsetParent);
+
+            scroll = function (c, a, b, i) {
+                i++;
+                if (i > 30) return;
+                c.scrollTop = a + (b - a) / 30 * i;
+                setTimeout(function () {
+                    scroll(c, a, b, i);
+                }, 20);
+            }
+            // start scrolling
+            scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+        }
+
+       
+
+    });
 
 });
