072781ea94fc923e15539721e2e79513af9adce4
Marcus Aurelius <markmayfield16@gmail.com>
2018-09-28 23:49:42 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -300,7 +300,7 @@ $(document).ready(function () {
         });
 
         ///////////////Database Quieries\\\\\\\\\\\\
-        let company = [{
+       let company = [{
                 name: "google",
                 location: "500 W 2nd St, Austin, TX 78701"
             },
@@ -346,13 +346,13 @@ $(document).ready(function () {
 
         for (i = 0; i < company.length; i++) {
             if (companyAdd === company[i].name) {
-                $("#stay22-script.s22obj.address").text(companyAdd);
-
+                $("#stay22-script.s22obj.address").text(companyAdd.value);
+console.log(this)
             }
         }
 
 
-        let search = "https://maps.googleapis.com/maps/api/geocode/json?address=" + "500 W 2nd St, Austin, TX 78701" + "&key=AIzaSyB5HFzXQptYixSzoZ8BjUGLQfVPTnKTbhw";
+        let search = "https://maps.googleapis.com/maps/api/geocode/json?address=" + "300 W 6th St, Austin, TX 78701" + "&key=AIzaSyB5HFzXQptYixSzoZ8BjUGLQfVPTnKTbhw";
         // function companyData(){
         //function getLatLng(){
 
@@ -365,36 +365,73 @@ $(document).ready(function () {
                 let longitude = response.results[0].geometry.location.lng
                 ///////////
 
-                let coordinates = JSON.stringify(response.results[0].geometry.lat)
+                
 
                 console.log(response);
-                console.log(coordinates)
-
-                console.log(latitude)
-
-                $.get({
-                        url: "https://api.stay22.com/v2/hotelscombined?latitude=" + latitude + "&longitude=" + longitude + "&radius=3000&checkin=2018-10-10&checkout=2018-10-14&minprice=0&maxprice=2000&language=en&currency=CAD&guests=2&rooms=1&limit=25&page=1",
-                        lat: latitude,
+                $("#second").click(function(){
+                    let checkIn= $("#start").val()
+                    console.log(checkIn)
+                    let checkOut = $("#end").val()
+                    console.log(checkOut)
+                    let maxP = $("#price option:selected").val()
+                    console.log(maxP)
+                    var settings22 = {
+                        width: '600px', // set the width in px or %
+                        height: '400px' // set the height in px or %
+                    };
+                
+                    // 2. Fill out your config here and the rest should work
+                    var s22obj = {
+                
+                        lat: latitude ,
                         lng: longitude,
-                        method: "GET"
-                    })
+                        maxprice: maxP,
+                        checkin: checkIn,
+                        checkout: checkOut,
+                        maincolor: '487fee', // your brand color in hex (without the #)
+                        markerimage: "www.google.com", // url of your logo or event image (in https)
+                        zoom: 15,
+                        hidebrandlogo: true,
+                        hideshare: true,
+                        hidefooter: true,
+                        hidefilters: true,
+                        hidenavbar: true,
+                        hidenavbuttons: true,
+                        hidesupport: true,
+                        fontcolor: '32fa95',
+                        hotelsfontcolor: '32fa95',
+                      };
+               
+                    // Leave this part intact
+                    var params22=''; for (var key in s22obj){if (params22){params22 +='&';}params22 +=key + '=' + encodeURIComponent(s22obj[key]);}var div22=document.getElementById('stay22-script'); div22.insertAdjacentHTML('afterend', '<iframe id="stay22-widget" width="' + settings22.width + '" height="' + settings22.height + '" src="' + 'https://www.stay22.com/embed/gm?' + params22 + '" frameborder="0"></iframe>');
+                    
 
-                    .then(function (data) {
-                        console.log(data);
-                        for (i = 0; i < data.results.length; i++) {
+                
 
-                            //console.log(data.results[i].images[1])
-                            $(".return-results").append(`
-                          <a href="${data.results[i].rooms[0].link}"><h3 class ="name">${data.results[i].name}</h3></a>
-                          
-                          <h4 class = "cost">$${data.results[i].max_rates.per_night}</h4>
-                          <h5 class = link>${data.results[i].rooms[0].link}</h5>`)
-                            console.log(data.results[i].name)
-                            console.log(data.results[i].max_rates.per_night)
-                            console.log(data.results[i].link)
-                            console.log(data);
-                        }
-                    })
+                        $.get({
+                                url: "https://api.stay22.com/v2/hotelscombined?latitude=" + latitude + "&longitude=" + longitude + "&radius=3000&checkin="+checkIn+"&checkout="+checkOut+"&minprice=0&maxprice="+maxP+"&language=en&currency=CAD&guests=2&rooms=1&limit=25&page=1",
+                                lat: latitude,
+                                lng: longitude,
+                                method: "GET"
+                            })
+
+                            .then(function (data) {
+                                
+                                console.log(data);
+                                for (i = 0; i < data.results.length; i++) {
+
+                                    //console.log(data.results[i].images[1])
+                                    $(".return-results").append(`
+                                <a href="${data.results[i].rooms[0].link}"><h3 class ="name">${data.results[i].name}</h3></a>
+                                
+                                <h4 class = "cost">$${data.results[i].max_rates.per_night}</h4>`)
+                                    console.log(data.results[i].name)
+                                    console.log(data.results[i].max_rates.per_night)
+                                    console.log(data.results[i].link)
+                                    console.log(data);
+                                }
+                            })
+                        })
             })
 
     });
